2b6c1bc28cd19305ca56726755a7b96094b30d77
Magdalena Kajszczak <41114872+magdakajszczak@users.noreply.github.com>
2025-05-12 21:36:49 +0200

    5# Generate new component

---
+++ src/app/name-displayer/name-displayer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-name-displayer',
+  imports: [],
+  templateUrl: './name-displayer.component.html',
+  styleUrl: './name-displayer.component.scss'
+})
+export class NameDisplayerComponent {
+
+}
