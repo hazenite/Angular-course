186a90989b49db267ffc4dd460e2b3b2d3ecc328
Magdalena Kajszczak <41114872+magdakajszczak@users.noreply.github.com>
2025-05-12 20:46:05 +0200

    3# Showing dynaminc variables

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
+  standalone: true,
   imports: [],
   templateUrl: './hello.component.html',
-  styleUrl: './hello.component.scss'
+  styleUrl: './hello.component.scss',
 })
 export class HelloComponent {
-
+  value = 'Loream ipsum';
+  getValue() {
+    return this.value;
+  }
 }
