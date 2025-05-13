23c29a77859a97fbf6156c74cc9c8ed4f49eb71f
Magdalena Kajszczak <41114872+magdakajszczak@users.noreply.github.com>
2025-05-13 20:32:55 +0200

    6# Communication between components

--- src/app/name-displayer/name-displayer.component.ts
+++ src/app/name-displayer/name-displayer.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-name-displayer',
   imports: [],
   templateUrl: './name-displayer.component.html',
-  styleUrl: './name-displayer.component.scss'
+  styleUrl: './name-displayer.component.scss',
 })
 export class NameDisplayerComponent {
+  @Input() name: string = '';
 
+  displayName: string = '';
+
+  showMyName() {
+    this.displayName = this.name;
+  }
 }
