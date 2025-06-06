9618558df1f44769858c0fb733512d1e17c7ba14
Magdalena Kajszczak <41114872+magdakajszczak@users.noreply.github.com>
2025-06-06 13:55:53 +0200

    14# Derivatives and lazy loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,18 @@ import { RandomNumberButtonComponent } from './random-number-button/random-numbe
 import { RandomNumbersComponent } from './random-numbers/random-numbers.component';
 import { CarComponent } from './car/car.component';
 import { CarsComponent } from './cars/cars.component';
+import { NgIf, NgFor, NgClass, NgStyle } from '@angular/common';
+import { FooComponent } from './foo/foo.component';
+
+type Box = {
+  r: number;
+  g: number;
+  b: number;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+};
 
 @Component({
   selector: 'app-root',
@@ -24,15 +36,74 @@ import { CarsComponent } from './cars/cars.component';
     // GrandparentComponent,
     // RandomNumberButtonComponent,
     // RandomNumbersComponent,
-    CarsComponent,
+    // CarsComponent,
+    NgIf,
+    NgFor,
+    NgClass,
+    NgStyle,
+    FooComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  boxses: Box[] = [];
+
+  isLogged: boolean | null = null;
+
+  left = 100;
+  top = 700;
+
+  isActive = false;
+  shouldBeVisable = false;
+  values: number[] = [1, 2, 3, 4, 5];
   title = 'agnular-course';
-  users: string[] = ['A', 'B', 'C', 'D', 'E'];
+  users: { id: number; name: string }[] = [
+    { id: 1, name: 'Marcin' },
+    { id: 2, name: 'Magda' },
+  ];
+
   getName() {
     return 'Lorem ipsum z metody getName w appComponent';
+  }
+  trackUsersFn(index: number, user: { id: number; name: string }) {
+    console.log('TRACK', index, user);
+    return user.id;
+  }
+
+  formatOdd(odd: boolean) {
+    if (odd) return 'is odd';
+    return 'is even';
+  }
+
+  getStyles() {
+    return {
+      position: `absolute`,
+      left: `${this.left}px`,
+      top: `${this.top}px`,
+    };
+  }
+
+  addBox() {
+    const newBox: Box = {
+      r: Math.round(Math.random() * 255),
+      g: Math.round(Math.random() * 255),
+      b: Math.round(Math.random() * 255),
+      x: Math.round(Math.random() * 255),
+      y: Math.round(Math.random() * 255),
+      width: Math.round(Math.random() * 255) + 100,
+      height: Math.round(Math.random() * 255) + 100,
+    };
+    this.boxses.push(newBox);
+  }
+
+  getStyles2(box: Box) {
+    return {
+      backgroundColor: `rgba(${box.r}, ${box.g}, ${box.b})`,
+      height: `${box.height}px`,
+      width: `${box.width}px`,
+      left: `${box.x}px`,
+      top: `${box.y}px`,
+    };
   }
 }
---
+++ src/app/foo/foo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-foo',
+  imports: [],
+  templateUrl: './foo.component.html',
+  styleUrl: './foo.component.scss'
+})
+export class FooComponent {
+
+}
